dea97ab3ba8ec3937e75346646250bfe158de185
Maksudul Hoque Aubhi <44749790+MaksudulAubhi@users.noreply.github.com>
2019-11-12 22:47:16 +0600

    delete

--- MediInfoManSystem/src/main/java/com/example/demo/service/DoctorService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.Doctor;
-
-public interface DoctorService {
-	public void saveUser(Doctor doctor);
-	public boolean isUserAlreadyPresent(Doctor doctor);
-}
--- MediInfoManSystem/src/main/java/com/example/demo/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.Employee;
-
-public interface EmployeeService {
-	public void saveUser(Employee employee);
-	public boolean isUserAlreadyPresent(Employee employee);
-	
-}
