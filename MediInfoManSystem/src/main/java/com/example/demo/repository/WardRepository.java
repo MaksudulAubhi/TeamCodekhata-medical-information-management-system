4e4fa072cc1333ec765203e4bd41fb742bff5d7f
Maksudul Hoque Aubhi <44749790+MaksudulAubhi@users.noreply.github.com>
2020-01-04 22:16:57 +0600

    delete

--- MediInfoManSystem/src/main/java/com/example/demo/repository/WardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Ward;
-
-public interface WardRepository extends JpaRepository<Ward, Integer> {
-	 Ward findById(int id);
-}
